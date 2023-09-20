8c983dee00ab4a5a9db690fb71e5a25edb87d3f2
Indomie 2 bungkus <145539548+universefactori@users.noreply.github.com>
2023-09-20 15:07:38 +0700

    Create biodata.kt

---
+++ biodata.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+  var nama = "Olivia Azzahra"
+  var kelas = "XI RPL 2"
+  var hobi = "baca webtoon"
+
+  println("nama: $nama")
+  println("kelas: $kelas")
+  println("hobi: $hobi")
+}
